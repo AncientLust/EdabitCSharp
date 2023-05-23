84b6bcd2d4267ea25b7aeedef8bff57b547eab0f
OnlyScam <aliakseikartashou@gmail.com>
2023-05-23 18:52:44 +0200

    Sentence Primeness has been solved.

---
+++ EdabitExercises/csharpexercises/SentencePrimeness.cs
@@ -0,0 +1,72 @@
+﻿using System.Text.RegularExpressions;
+
+public class SentencePrimenessClass
+{
+    public static string SentencePrimeness(string sentence)
+    {
+        // Filter our in the sentence non-alphanumeric values
+        Regex regex = new Regex("[^a-zA-Z0-9 ]");
+        sentence = regex.Replace(sentence, "");
+
+        // Calculate the sum of the sentence
+        int sum = CalculateSentenceSum(sentence);
+
+        // If sum is prime return smthng
+        if (IsPrime(sum)) return "Prime Sentence";
+        
+        // Split the sentence by spaces
+        List<string> words = sentence.Split(' ').ToList();
+        foreach (string word in words)
+        {
+            sum = CalculateSentenceSum(sentence.Replace(word, ""));
+            if (IsPrime(sum)) return $"Almost Prime Sentence ({word})";
+        }
+
+        return "Composite Sentence";
+    }
+
+    private static int CalculateSentenceSum(string sentence)
+    {
+        // Filter out spaces
+        sentence = sentence.Replace(" ", "");
+
+        int sum = 0;
+        foreach (char c in sentence)
+        {
+            if (char.IsLetter(c))
+            {
+                // Convert letter to its corresponding value
+                int letterValue = char.ToLower(c) - 'a' + 1;
+                sum += letterValue;
+            }
+            else if (char.IsDigit(c))
+            {
+                // Convert digit to its literal value
+                int digitValue = c - '0';
+                sum += digitValue;
+            }
+        }
+
+        return sum;
+    }
+
+    public static bool IsPrime(int number)
+    {
+        if (number <= 1)
+            return false;
+
+        if (number == 2)
+            return true;
+
+        if (number % 2 == 0)
+            return false;
+
+        for (int i = 3; i <= Math.Sqrt(number); i += 2)
+        {
+            if (number % i == 0)
+                return false;
+        }
+
+        return true;
+    }
+}
---
+++ UnitTestsProject/UnitTests/SentencePrimeness.cs
@@ -0,0 +1,20 @@
+﻿namespace TestingProject
+{
+    public partial class SentencePrimenessTests
+    {
+        [TestCase("Help me!", ExpectedResult = "Prime Sentence", Description = "Example #1")]
+        [TestCase("42 is THE aNsWeR...", ExpectedResult = "Almost Prime Sentence (aNsWeR)", Description = "Example #2")]
+        [TestCase("Did you Smoke?", ExpectedResult = "Composite Sentence", Description = "Example #3")]
+        [TestCase("She SellS SeaShellS by the SeaShore", ExpectedResult = "Prime Sentence")]
+        [TestCase("Lorem. Ipsum. Dolor. Sit. Amet.", ExpectedResult = "Almost Prime Sentence (Lorem)")]
+        [TestCase("three fASt hUNgry aniMALs -aNd- 3 slow faTTy kiDS", ExpectedResult = "Almost Prime Sentence (aniMALs)")]
+        [TestCase("This is a 'Prime' Sentence", ExpectedResult = "Composite Sentence")]
+        [TestCase("this is a composite sentence", ExpectedResult = "Almost Prime Sentence (this)")]
+        [TestCase("Primes, PRIMES EVERYWHERE!", ExpectedResult = "Composite Sentence")]
+        [TestCase("10 test cases are enough, this is the last one!", ExpectedResult = "Prime Sentence")]
+        public static string TestSolution(string str)
+        {
+            return SentencePrimenessClass.SentencePrimeness(str);
+        }
+    }
+}
