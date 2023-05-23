810ca37d5defa35d68638106192535683f9feb43
OnlyScam <aliakseikartashou@gmail.com>
2023-05-23 20:25:55 +0200

    Prefix Notation Evaluation has been solved.

---
+++ EdabitExercises/csharpexercises/PrefixNotationEvaluation.cs
@@ -0,0 +1,60 @@
+﻿using System.Data;
+using System.Text;
+
+class PrefixNotationEvaluation
+{
+    public static int Prefix(string exp)
+    {
+        string[] args = exp.Split(' ');
+
+        // Collect char operators
+        List<char> operators = new List<char>();
+        int i = 0;
+        while (i < args.Length && !int.TryParse(args[i], out int intValue))
+        {
+            if (args[i].Length == 1)
+                operators.Add(args[i][0]);
+            else
+            {
+                Console.WriteLine($"Invalid operator: {args[i]}");
+                return 0;
+            }
+            i++;
+        }
+
+        // Collect numeric operands
+        List<int> operands = new List<int>();
+        while (i < args.Length)
+        {
+            if (int.TryParse(args[i], out int operandValue))
+                operands.Add(operandValue);
+            else
+            {
+                Console.WriteLine($"Invalid operand: {args[i]}");
+                return 0;
+            }
+            i++;
+        }
+
+        // Validate operators and operands count
+        if (operators.Count != operands.Count - 1)
+        {
+            Console.WriteLine("Mismatched number of operators and operands");
+            return 0;
+        }
+
+        // Initialize a StringBuilder to build the expression
+        StringBuilder expressionBuilder = new StringBuilder(operands[0].ToString());
+
+        // Append the remaining operators and operands
+        for (i = 0; i < operators.Count; i++)
+        {
+            expressionBuilder.Append(operators[i]);
+            expressionBuilder.Append(operands[i + 1]);
+        }
+
+        // Compute the expression
+        var result = new DataTable().Compute(expressionBuilder.ToString(), null);
+        return Convert.ToInt32(result);
+    }
+}
---
+++ UnitTestsProject/UnitTests/PrefixNotationEvaluation.cs
@@ -0,0 +1,24 @@
+﻿namespace TestingProject
+{
+    public partial class PrefixNotationEvaluationTests
+    {
+        [TestCase("- -14 -15", ExpectedResult = 1)]
+        [TestCase("+ 12 13", ExpectedResult = 25)]
+        [TestCase("* 3 3", ExpectedResult = 9)]
+        [TestCase("+ -15 15", ExpectedResult = 0)]
+        [TestCase("- 2200 100", ExpectedResult = 2100)]
+        [TestCase("/ 1000 10", ExpectedResult = 100)]
+        [TestCase("+ 20 20", ExpectedResult = 40)]
+        [TestCase("* - 8 6 10", ExpectedResult = 38)]
+        [TestCase("+ 13 100", ExpectedResult = 113)]
+        [TestCase("* / 40 4 3", ExpectedResult = 53)]
+        [TestCase("- + 200 300 500", ExpectedResult = 400)]
+        [TestCase("+ - * / 100 10 4 100 20", ExpectedResult = 90)]
+        [TestCase("* + 20 2 5", ExpectedResult = 45)]
+        [TestCase("/ + * 12 8 2 2", ExpectedResult = 6)]
+        public static int TestSolution(string exp)
+        {
+            return PrefixNotationEvaluation.Prefix(exp);
+        }
+    }
+}
