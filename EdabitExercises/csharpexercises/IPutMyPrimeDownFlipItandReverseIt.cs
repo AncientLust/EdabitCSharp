d734817b6522b14fb4eb88fcf2110caa1e7dd58b
OnlyScam <aliakseikartashou@gmail.com>
2023-05-24 22:12:22 +0200

    I Put My Prime Down, Flip It and Reverse It has been solved.

---
+++ EdabitExercises/csharpexercises/IPutMyPrimeDownFlipItandReverseIt.cs
@@ -0,0 +1,38 @@
+﻿class IPutMyPrimeDownFlipItandReverseIt
+{
+    public static string Bemirp(int n)
+    {
+        if (IsBemirp(n)) return "B";
+        if (IsEmirp(n)) return "E";
+        if (IsPrime(n)) return "P";
+        return "C";
+    }
+
+    public static bool IsPrime(int number)
+    {
+        if (number < 2)
+            return false;
+
+        for (int i = 2; i <= Math.Sqrt(number); i++)
+        {
+            if (number % i == 0)
+                return false;
+        }
+
+        return true;
+    }
+
+    private static bool IsEmirp(int n)
+    {
+        int reversedNumber = int.Parse(n.ToString().Reverse().ToArray());
+        return IsPrime(n) && IsPrime(reversedNumber) && n != reversedNumber;
+    }
+
+    private static bool IsBemirp(int n)
+    {
+        // Flip the number
+        string flippedNumber = n.ToString().Replace("6", "T").Replace("9", "6").Replace("T", "9");
+
+        return IsEmirp(n) && IsPrime(int.Parse(flippedNumber));
+    }
+}
---
+++ UnitTestsProject/UnitTests/IPutMyPrimeDownFlipItandReverseIt.cs
@@ -0,0 +1,20 @@
+﻿namespace TestingProject
+{
+    public partial class IPutMyPrimeDownFlipItandReverseItTests
+    {
+        [TestCase(101, ExpectedResult = "P")]
+        [TestCase(1011, ExpectedResult = "C")]
+        [TestCase(1069, ExpectedResult = "E")]
+        [TestCase(1061, ExpectedResult = "B")]
+        [TestCase(198101, ExpectedResult = "C")]
+        [TestCase(1009, ExpectedResult = "E")]
+        [TestCase(10091, ExpectedResult = "B")]
+        [TestCase(18616061, ExpectedResult = "B")]
+        [TestCase(10909, ExpectedResult = "E")]
+        [TestCase(16069, ExpectedResult = "P")]
+        public static string TestSolution(int n)
+        {
+            return IPutMyPrimeDownFlipItandReverseIt.Bemirp(n);
+        }
+    }
+}
