f8b6c86406676a90395c565539d80738a210f194
OnlyScam <aliakseikartashou@gmail.com>
2023-06-03 17:56:59 +0200

    Simplified Proper Fractions task has been solved.

---
+++ EdabitExercises/csharpexercises/SimplifiedProperFractions.cs
@@ -0,0 +1,29 @@
+﻿using System.Text.RegularExpressions;
+
+public class SimplifiedProperFractions
+{
+    public static int SimPropFrac(int maxDen)
+    {
+        int result = 0;
+        for (int i = 2; i <= maxDen; i++)
+            result += Phi(i);
+        return result;
+    }
+
+    private static int Phi(int n)
+    {
+        int result = n;
+        for (int p = 2; p * p <= n; ++p)
+        {
+            if (n % p == 0)
+            {
+                while (n % p == 0)
+                    n /= p;
+                result -= result / p;
+            }
+        }
+        if (n > 1)
+            result -= result / n;
+        return result;
+    }
+}
---
+++ UnitTestsProject/UnitTests/SimplifiedProperFractions.cs
@@ -0,0 +1,16 @@
+﻿namespace TestingProject
+{
+    public partial class SimplifiedProperFractionsTests
+    {
+        [TestCase(10, ExpectedResult = 31)]
+        [TestCase(2, ExpectedResult = 1)]
+        [TestCase(30, ExpectedResult = 277)]
+        [TestCase(100, ExpectedResult = 3043)]
+        [TestCase(56, ExpectedResult = 963)]
+        public static int TestCode(int num)
+        {
+            Console.WriteLine($"Input: {num}");
+            return SimplifiedProperFractions.SimPropFrac(num);
+        }
+    }
+}
