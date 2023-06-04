91f89ccdb4f7c58fc33d53d8edc89907a5db69aa
OnlyScam <aliakseikartashou@gmail.com>
2023-06-05 00:21:20 +0200

    The Centrifuge Problem has been solved.

---
+++ EdabitExercises/csharpexercises/TheCentrifugeProblem.cs
@@ -0,0 +1,41 @@
+﻿public class TheCentrifugeProblem
+{
+    public static bool Centrifuge(int n, int k)
+    {
+        List<int> primes = PrimeFactors(n);
+
+        bool[] expressible = new bool[n + 1];
+        expressible[0] = true;
+
+        foreach (var prime in primes)
+        {
+            for (int i = prime; i <= n; i++)
+            {
+                expressible[i] |= expressible[i - prime];
+            }
+        }
+
+        return expressible[k] && expressible[n - k];
+    }
+
+    private static List<int> PrimeFactors(int n)
+    {
+        var primes = new List<int>();
+
+        for (int i = 2; i * i <= n; i++)
+        {
+            while (n % i == 0)
+            {
+                primes.Add(i);
+                n /= i;
+            }
+        }
+
+        if (n > 1)
+        {
+            primes.Add(n);
+        }
+
+        return primes;
+    }
+}
---
+++ UnitTestsProject/UnitTests/TheCentrifugeProblem.cs
@@ -0,0 +1,27 @@
+﻿namespace TestingProject
+{
+    public partial class TheCentrifugeProblemTests
+    {
+        [TestCase(5, 1, ExpectedResult = false)]
+        [TestCase(12, 7, ExpectedResult = true)]
+        [TestCase(1, 1, ExpectedResult = false)]
+        [TestCase(21, 18, ExpectedResult = true)]
+        [TestCase(1, 0, ExpectedResult = false)]
+        [TestCase(4, 2, ExpectedResult = true)]
+        [TestCase(5, 3, ExpectedResult = false)]
+        [TestCase(21, 13, ExpectedResult = false)]
+        [TestCase(3, 3, ExpectedResult = true)]
+        [TestCase(50, 1, ExpectedResult = false)]
+        [TestCase(8, 6, ExpectedResult = true)]
+        [TestCase(21, 20, ExpectedResult = false)]
+        [TestCase(9, 5, ExpectedResult = false)]
+        [TestCase(2, 1, ExpectedResult = false)]
+        [TestCase(59, 59, ExpectedResult = true)]
+        [TestCase(11, 4, ExpectedResult = false)]
+        public static bool TestCentrifuge(int n, int k)
+        {
+            Console.WriteLine($"Input: n={n}, k={k}");
+            return TheCentrifugeProblem.Centrifuge(n, k);
+        }
+    }
+}
